31a4b7f2bd3f08f82bea0568f949cfa2d0f5999c
Bontle <126960560+bontlenkoale1@users.noreply.github.com>
2024-02-24 23:39:27 +0200

    footer.js

--- footer.js
+++ footer.js
@@ -1,3 +1,10 @@
 <script>
   const navlink = document.querySelector; menuBtn.addEventsListener("click");
+ <section class="footer">
+      <p>
+        Physical Address: Sandton City Centre Management Offices, Atrium on
+        5th,Sandton City <br />Fax: 011 883 0978 <br />Postal: PO Box 78100,
+        Sandton 2146 <br />Telephone: 011 217 6000
+      </p>
+    </section>
 </script>;
